2977396905dba18daf8afa659341be9fed81aac6
komalkoutilya <komalkoutilya@gmail.com>
2026-04-25 18:25:04 +0530

    resolve 01_variables > all

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -1,8 +1,7 @@
 fn main() {
     // TODO: Change the line below to fix the compiler error.
-    let x;
-
-    if x == 10 {
+    let x: u8 = 0;
+    if 10 == x {
         println!("x is ten!");
     } else {
         println!("x is not ten!");
